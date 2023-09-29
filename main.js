b583be6d5b7d58a49ac1cb7407cf5429600075aa
Home PC <vfxprojectguy@gmail.com>
2023-09-28 23:17:19 -0700

    edit main.js function remove unused code add more names and occupations

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-const names = ["Alice", "Bob", "Carol","James","Emily","John","Ethan","Hanna" ];
+const names = ["Alice", "Bob", "Carol","James","Emily","John","Ethan","Hanna", "Liam", "Emma", "Noah", "Olivia", "Oliver", "Ava", "Elijah", "Charlotte", "James", "Sophia",
+"Benjamin", "Amelia", "Lucas", "Isabella", "Henry", "Mia", "Alexander", "Harper", "Sebastian", "Evelyn",
+"William", "Abigail", "Michael", "Aria", "Ethan", "Riley", "Daniel", "Grace", "Matthew", "Chloe",
+"Logan", "Ella", "David", "Lily", "Jackson", "Zoe", "Joseph", "Madison", "Samuel", "Layla",
+"Henry", "Scarlett", "Owen", "Nora", "Carter", "Luna", "John", "Mila", "Jack", "Sofia" ];
 const occupations = [
-  "Writer",
+  "Software Developer",
+  "Graphic Designer",
+  "Doctor",
   "Teacher",
-  "Programmer",
-  "Banker",
-  "Clinician",
-  "Baker",
+  "Engineer",
+  "Nurse",
+  "Architect",
+  "Web Developer",
+  "Accountant",
+  "Electrician",
+  "Plumber",
+  "Chef",
+  "Writer",
+  "Photographer",
+  "Marketing Manager",
+  "Data Analyst",
+  "Lawyer",
+  "Mechanic",
+  "Dentist",
+  "Artist",
+  "Psychologist",
+  "Social Worker",
+  "Veterinarian",
+  "Financial Analyst",
+  "Human Resources Manager",
+  "Translator",
+  "Physiotherapist",
+  "Pharmacist",
+  "Meteorologist",
+  "Pilot",
   "Hair stylist",
 ];
 const maxFreelancer = 15;
@@ -15,66 +43,47 @@ const freelancers = [
   { name: "Bob", occupation: "Teacher", price: 50 },
 ]; 
 
-// Call the renderFreelancers function initially
-renderFreelancers();
+render();
 
-// Set an interval to add new freelancers every 5 seconds (5000 milliseconds)
-const addFreelancer =  setInterval(generateRandomFreelancer, 2000);
+ const addfreelancer = setInterval(generateRandom, 5000)
 
-
-//update the DOM to reflect the current state.
-function renderFreelancers() {
+function render () {
   const freelancerList = document.querySelector("#freelancer-list");
-  freelancerList.innerHTML = "";
-  // Loop through the freelancers array and create HTML elements for each freelancer
-  for (const freelancer of freelancers) {
-    const element = document.createElement("li");
-    element.innerHTML = `${freelancer.name}, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${freelancer.occupation}, &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Starting at $${freelancer.price}`;
-
+  freelancerList.innerHTML='';
+  for(const freelancer of freelancers) {
+    const element = document.createElement('li');
+    element.innerHTML = `${freelancer.name},&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${freelancer.occupation},&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Starting at $${freelancer.price}`
+    //console.log('select element:',element)
     freelancerList.appendChild(element);
   }
 }
 
-
-//add random name, occupation and price to the freelancers array. 
-function generateRandomFreelancer() {
-
+function generateRandom () {
   const randomName = names[Math.floor(Math.random() * names.length)];
   const randomOccupation = occupations[Math.floor(Math.random() * occupations.length)];
-  const randomPrice = Math.floor(Math.random() * 80) + 30; // Random price between 30 and 100
+  const rendomPrice = Math.floor(Math.random() * 80) + 30;
 
-  const newFreelancer = {
-    name: randomName,
-    occupation: randomOccupation,
-    price: randomPrice,
-  };
+     const newFreelancer ={
+      name : randomName,
+      occupation : randomOccupation,
+      price : rendomPrice,
+     }
+     freelancers.push(newFreelancer)
+     console.log('new random freelancer',newFreelancer)
+     render();
+     calculateAveragePrice();
 
-  freelancers.push(newFreelancer);
-
-  // Call the renderFreelancers function to update the displayed list
-  renderFreelancers();
-   
-  calculateAveragePrice();
-   // stop adding freelancers if reached the max number of freelancers.
-  if(freelancers.length >= maxFreelancer) {
-    clearInterval(addFreelancer)
-  }
+    //  if(freelancers.length === 50 ) {
+    //   clearInterval(addfreelancer)
+    //  }
 }
 
+function calculateAveragePrice () {
+  const averagePrice = document.querySelector('#average-price');
 
-
-function calculateAveragePrice() {
-  const averagePriceMessage = document.querySelector("#average-price");
-  if (freelancers.length === 0) {
-    averagePriceMessage.textContent = "No freelancers available";
-    return;
-  }
-
-  const totalPrices = freelancers.reduce((sum, freelancer) => sum + freelancer.price, 0);
-  const averagePrice = totalPrices / freelancers.length;
-
-  averagePriceMessage.textContent = `Average starting price: $${averagePrice.toFixed(2)}`;
+  const totalprice = freelancers.reduce((acc,cur) => acc + cur.price, 0);
+  let averagePriceamount = totalprice / freelancers.length;
+  
+  averagePrice.textContent = `Average starting price: $${averagePriceamount.toFixed(2)}`
 }
-
-// Call the calculateAveragePrice function initially
 calculateAveragePrice();
